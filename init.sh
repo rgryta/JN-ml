600499333493ad6e906accd2c3ccb75a235deeb2
rgryta <radek.gryta@gmail.com>
2022-07-13 17:02:48 +0200

    Fixed sudo script

--- init.sh
+++ init.sh
@@ -3,13 +3,12 @@ sudo fallocate -l 10GB /mnt/10GB.swap
 sudo chmod 600 /mnt/10GB.swap
 sudo mkswap /mnt/10GB.swap
 
-sudo su
-echo "/mnt/10GB.swap swap swap defaults 0 0" >> /etc/fstab
-exit
-
+echo "/mnt/10GB.swap swap swap defaults 0 0" | sudo tee -a /etc/fstab
 
 echo "Adding nvidia as default container runtime for building containers"
-jq '.["default-runtime"] = "nvidia"' /etc/docker/daemon.json > tmp.$$.json && mv tmp.$$.json /etc/docker/daemon.json
+sudo apt-get install -y jq
+jq '.["default-runtime"] = "nvidia"' /etc/docker/daemon.json > tmp.$$.json && sudo mv -f tmp.$$.json /etc/docker/daemon.json
+rm tmp.*.json
 
 echo "Fix GPG commit signing"
 export GPG_TTY=$(tty)
